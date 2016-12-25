83d57a56ec07f2cc03f6bf482ce929dde555e826
Jake Fenton <bocajnotnef@gmail.com>
2016-12-25 18:43:07 -0500

    moved output to be before ssh blocks output...

--- helpme.sh
+++ helpme.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ssh -nNT -R 5502:localhost:22 esw@highnoiseratio.org
 echo "SSH tunnel running, access port 5502 on remote server"
 echo "To end tunnel, hit ctrl-c"
+ssh -nNT -R 5502:localhost:22 esw@highnoiseratio.org
