13031fd01fac73df77ed45e060d7b38df5134dcb
Tess Huelskamp <tess.huelskamp@gmail.com>
2016-12-27 08:02:10 -0500

    outline. commiting so that I can switch to a new branch.
    good thing I plan on squashing these...

---
+++ lesson1/lesson1.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+
+
+intro="""
+Hello $KID! 
+
+Jake asked me to look over this intro and I thought 
+that it would be good to add a lesson abou types. 
+
+Types are, to be completely honest, are reallllly boring
+but they're important to grasp the basic types as they're
+useful to know later on. 
+
+I'll try to make this fun. 
+
+>>> Press enter to continue. <<<
+"""
+
+print(intro) 
+
+x = input() 
+
+print("idk") 
+
+
