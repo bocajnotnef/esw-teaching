2eccdd828e4f9a5c2f17fc26c4a8b312f9c09660
Tess Huelskamp <tess.huelskamp@gmail.com>
2016-12-27 09:56:31 -0500

    this looks a lot better and is lot more prone to transformations (shifts, skews, rotations) which sound fun to mess with

--- todo/turtle/block-S2.py
+++ todo/turtle/block-S2.py
@@ -11,26 +11,27 @@ the lengths of the S.
 This makes it easy to scale either the x, or y coordinates. 
 """
 
+
+
 #import the turtle module
 #and be able to use the commands
 import turtle
-
-#list of points from start to end
-DIAG=75
-
-CAP_WIDTH=100
-OUTSIDE_CAP=100
-INSIDE_CAP=50
-SMALL_INSIDE_WIDTH=200
-LARGE_INSIDE_WIDTH=250
-INSIDE_VERTICAL=100
-OUTSIDE_VERTICAL=200
-OUTSIDE_LINES=300
 
 #make the "turtle object"
 #this is our "turtle" that will
 #run around the screen
 sparty = turtle.Turtle()
+
+#the points that sparty will travel to in order 
+#to make the block S
+points=[
+        (-200.00,250.00), (100.00,250.00), (153.03,196.97), (153.03,96.97), 
+        (53.03,96.97), (53.03,146.97), (-146.97,146.97), (-146.97,21.97), 
+        (103.03,21.97), (156.07,-31.07), (156.07,-256.07), (103.03,-309.10), 
+        (-196.97,-309.10), (-250.00,-256.07), (-250.00,-156.07), (-150.00,-156.07), 
+        (-150.00,-206.07), (50.00,-206.07), (50.00,-81.07), (-200.00,-81.07), 
+        (-253.03,-28.03), (-253.03,196.97), (-200.00,250.00)
+        ]
 
 #set the color to be THE spartan green
 sparty.pencolor("#18453B")
@@ -38,125 +39,13 @@ sparty.pencolor("#18453B")
 #Put the pen "up" so that when we move we dont draw
 sparty.penup()
 
-#move sparty to the top left since that's where were starting
-sparty.setx(-200)
-sparty.sety(250)
+#move sparty to starting point of the block S
+sparty.goto(points[0]) 
 
-#put the pen "down" so that we can start drawing
+#set the pen down and set the width 
 sparty.pendown()
-print(sparty.position())
+sparty.width(30)
 
-#Set the pen width
-sparty.width(20)
-
-#Draw the top part of the block S
-sparty.forward(OUTSIDE_LINES)
-sparty.position()
-
-#right 45 degrees and draw the next part
-sparty.right(45)
-sparty.forward(DIAG)
-sparty.position()
-
-#drawing down
-sparty.right(45)
-sparty.forward(OUTSIDE_CAP)
-sparty.position()
-
-#drawing left
-sparty.right(90)
-sparty.forward(CAP_WIDTH)
-sparty.position()
-
-#draw up from the edge of the S
-sparty.right(90)
-sparty.forward(INSIDE_CAP)
-sparty.position()
-
-#angle back
-sparty.left(90)
-sparty.forward(SMALL_INSIDE_WIDTH)
-sparty.position()
-
-#draw down the inside of the top of the S
-sparty.left(90)
-sparty.forward(INSIDE_VERTICAL)
-sparty.position()
-
-
-#draw the middle of the S
-sparty.left(90)
-sparty.forward(LARGE_INSIDE_WIDTH)
-sparty.position()
-
-#angle down
-sparty.right(45)
-sparty.forward(DIAG)
-sparty.position()
-
-#angle down again
-sparty.right(45)
-sparty.forward(OUTSIDE_VERTICAL)
-sparty.position()
-
-#bottom right diag
-sparty.right(45)
-sparty.forward(DIAG)
-sparty.position()
-
-#bottom of S
-sparty.right(45)
-sparty.forward(OUTSIDE_LINES)
-sparty.position()
-
-#bottom left diag
-sparty.right(45)
-sparty.forward(DIAG)
-sparty.position()
-
-sparty.right(45)
-sparty.forward(OUTSIDE_CAP)
-sparty.position()
-
-sparty.right(90)
-sparty.forward(CAP_WIDTH)
-sparty.position()
-
-sparty.right(90)
-sparty.forward(INSIDE_CAP)
-sparty.position()
-
-sparty.left(90)
-sparty.forward(SMALL_INSIDE_WIDTH)
-sparty.position()
-
-#draw up the inside of the bottom of the S
-sparty.left(90)
-sparty.forward(INSIDE_VERTICAL)
-sparty.position()
-
-sparty.left(90)
-sparty.forward(LARGE_INSIDE_WIDTH)
-sparty.position()
-
-#diag up
-sparty.right(45)
-sparty.forward(DIAG)
-sparty.position()
-
-#one last up
-sparty.right(45)
-sparty.forward(OUTSIDE_VERTICAL)
-sparty.position()
-
-#last line
-sparty.right(45)
-sparty.forward(DIAG)
-sparty.position()
-
-
-
-#waiting for user input to end the program
-input()
-
+for coord in points:
+    sparty.goto(coord)
 
